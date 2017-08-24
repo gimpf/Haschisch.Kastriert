1c6687bff990fbfbe8e7fce41afa102e64776bb8
Markus Grüneis <gimpf@gimpf.org>
2017-08-24 23:20:22 +0200

    port to .NET Standard 2.0 and .NET Core 2.0
    - recreate library projects as .NETStandard
    - extract Haschisch.Benchmarks.Spec from Haschisch.Benchmarks, to
      allow running benchmarks on regular CLR (it would otherwise fail
      as assembly not supporting the .NET Framework, because it would
      only be marked as compatible with .NET Core)

---
+++ Haschisch/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCopyright("Released under terms of CC0 by Markus Grüneis <gimpf@gimpf.org>")]
+
+[assembly: InternalsVisibleTo("Haschisch.Educational")]
+[assembly: InternalsVisibleTo("Haschisch.Benchmarks")]
+[assembly: InternalsVisibleTo("Haschisch.Benchmarks.Spec")]
+[assembly: InternalsVisibleTo("Haschisch.Tests")]
